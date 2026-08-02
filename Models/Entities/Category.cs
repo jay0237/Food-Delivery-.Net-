7816400d514b6898938c6e1dbf07dc03d9a448be
jay0237 <jayjoshi16534@gmail.com>
2026-08-02 22:18:35 +0530

    create Enitity

---
+++ Models/Entities/Category.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodOrderingSystem.Models.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        public string? ImageUrl { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
