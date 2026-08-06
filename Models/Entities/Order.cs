f67e45fc1ad8ecc197aef55109953331af6002cc
jay0237 <jayjoshi16534@gmail.com>
2026-08-06 22:24:17 +0530

    add Order Entity

---
+++ Models/Entities/Order.cs
@@ -0,0 +1,16 @@
+public class Order
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public User User { get; set; } = null!;
+
+    public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+    public decimal TotalAmount { get; set; }
+
+    public string Status { get; set; } = "Pending";
+
+    public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
