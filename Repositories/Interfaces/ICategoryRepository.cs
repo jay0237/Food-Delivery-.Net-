f4f16c1ae04ca63dc8f1a630ee61951a2dc42cb9
jay0237 <jayjoshi16534@gmail.com>
2026-08-07 22:53:58 +0530

    add category(repo) structure

---
+++ Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+using FoodOrderingSystem.Models.Entities;
+
+namespace FoodOrderingSystem.Repositories.Interfaces;
+
+public interface ICategoryRepository
+{
+    Task<IEnumerable<Category>> GetAllAsync();
+    Task<Category?> GetByIdAsync(int id);
+    Task AddAsync(Category category);
+    Task UpdateAsync(Category category);
+    Task DeleteAsync(int id);
+}
