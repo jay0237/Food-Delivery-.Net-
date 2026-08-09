37a66c6c904d33e7d326a12400d59d5726af05f5
jay0237 <jayjoshi16534@gmail.com>
2026-08-09 21:34:29 +0530

    Add CRUD Operations

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,24 +1,98 @@
+using FoodOrderingSystem.Models.Entities;
 using FoodOrderingSystem.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoodOrderingSystem.Controllers;
 
-[Route("category")]
-[Route("categories")]
-public class CategoriesController : Controller
+public class CategoryController : Controller
 {
     private readonly ICategoryService _categoryService;
 
-    public CategoriesController(ICategoryService categoryService)
+    public CategoryController(ICategoryService categoryService)
     {
         _categoryService = categoryService;
     }
 
-    [HttpGet("")]
+    // GET: /Category
     public async Task<IActionResult> Index()
     {
         var categories = await _categoryService.GetAllAsync();
 
         return View(categories);
+    }
+
+    // GET: /Category/Create
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: /Category/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Category category)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(category);
+        }
+
+        await _categoryService.AddAsync(category);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: /Category/Edit/1
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var category = await _categoryService.GetByIdAsync(id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    // POST: /Category/Edit
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(Category category)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(category);
+        }
+
+        await _categoryService.UpdateAsync(category);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: /Category/Delete/1
+    [HttpGet]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var category = await _categoryService.GetByIdAsync(id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    // POST: /Category/Delete/1
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        await _categoryService.DeleteAsync(id);
+
+        return RedirectToAction(nameof(Index));
     }
 }
