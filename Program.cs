5bb265c6f8ea28ddfce2aee5a57ef2ed3087de73
jay0237 <jayjoshi16534@gmail.com>
2026-08-10 22:42:56 +0530

    add food id

--- Program.cs
+++ Program.cs
@@ -6,9 +6,6 @@ using FoodOrderingSystem.Services.Implementations;
 using FoodOrderingSystem.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
-builder.Services.AddScoped<IFoodRepository, FoodRepository>();
-builder.Services.AddScoped<IFoodService, FoodService>();
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -17,6 +14,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IFoodRepository, FoodRepository>();
+builder.Services.AddScoped<IFoodService, FoodService>();
 
 var app = builder.Build();
 
