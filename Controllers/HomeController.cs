32c66d4def8e15d36eff82e9944294031817c898
jay0237 <jayjoshi16534@gmail.com>
2026-09-11 23:02:50 +0530

    add shell points

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,41 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FoodOrderingSystem.Models;
+using FoodOrderingSystem.Services.Interfaces;
 
 namespace FoodOrderingSystem.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly IFoodService _foodService;
+    private readonly ICategoryService _categoryService;
+
+    public HomeController(IFoodService foodService, ICategoryService categoryService)
+    {
+        _foodService = foodService;
+        _categoryService = categoryService;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        return ViewAsync();
+    }
+
+    private async Task<IActionResult> ViewAsync()
+    {
+        var foods = (await _foodService.GetAllAsync())
+            .Where(food => food.IsAvailable)
+            .OrderByDescending(food => food.CreatedAt)
+            .ToList();
+
+        var categories = (await _categoryService.GetAllAsync()).ToList();
+
+        return View(new HomeViewModel
+        {
+            Categories = categories,
+            FeaturedFoods = foods.Take(6).ToList(),
+            MenuCount = foods.Count
+        });
     }
 
     public IActionResult Privacy()
